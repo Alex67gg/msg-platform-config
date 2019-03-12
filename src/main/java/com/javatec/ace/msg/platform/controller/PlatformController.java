0267de0714d131686606078cec8a15c25b15e7de
Alex67gg <2133978075@qq.com>
2019-03-12 16:04:47 +0800

    Hello World!

--- src/main/java/com/javatec/ace/msg/platform/controller/PlatformController.java
+++ src/main/java/com/javatec/ace/msg/platform/controller/PlatformController.java
@@ -1,7 +1,15 @@
 package com.javatec.ace.msg.platform.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/base")
 public class PlatformController {
+    @GetMapping("/name/{name}")
+    public String getName(@PathVariable(value = "name") String name){
+        return name+"Hello World!";
+    }
 }
